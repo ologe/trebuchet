2d0e152d0cdd7ede58c4648b2d47be03a1c142d2
Eugeniu Olog <eugeniu.olog@gmail.com>
2020-07-04 16:58:48 +0200

    gradle plugin update to 4.0.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,7 +13,7 @@ kotlinDslPluginOptions {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:4.0.0-rc01")
+    implementation("com.android.tools.build:gradle:4.0.0")
     implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.3.72")
     implementation("net.java.dev.jna:jna:4.2.2")
 }
